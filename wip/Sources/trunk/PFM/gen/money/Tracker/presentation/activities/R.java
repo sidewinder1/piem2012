3953f6889587a1225eb7650b451acc0651e6e315
locnd01006@fpt.edu.vn <locnd01006@fpt.edu.vn@15378052-1c41-e357-49ac-df63ba3f61f7>
2012-09-27 13:42:00 +0000

    Update code for View Schedule function

--- wip/Sources/trunk/PFM/gen/money/Tracker/presentation/activities/R.java
+++ wip/Sources/trunk/PFM/gen/money/Tracker/presentation/activities/R.java
@@ -26,22 +26,24 @@ public final class R {
         public static final int tab_unselected=0x7f020009;
     }
     public static final class id {
-        public static final int addSchedule=0x7f08000c;
+        public static final int addSchedule=0x7f08000e;
+        public static final int cancelBtn=0x7f080003;
+        public static final int doneBtn=0x7f080002;
         public static final int mainLayout=0x7f080001;
-        public static final int menu_settings=0x7f08000f;
-        public static final int periodic=0x7f080002;
-        public static final int schedule_end_date=0x7f080004;
-        public static final int schedule_item_add=0x7f080009;
-        public static final int schedule_item_category=0x7f080007;
-        public static final int schedule_item_list=0x7f080006;
-        public static final int schedule_item_price=0x7f080008;
-        public static final int schedule_item_remove=0x7f08000a;
-        public static final int schedule_start_date=0x7f080003;
-        public static final int schedule_total_budget=0x7f080005;
-        public static final int tabsLayout=0x7f08000d;
-        public static final int tabsText=0x7f08000e;
+        public static final int menu_settings=0x7f080011;
+        public static final int periodic=0x7f080004;
+        public static final int schedule_end_date=0x7f080006;
+        public static final int schedule_item_add=0x7f08000b;
+        public static final int schedule_item_category=0x7f080009;
+        public static final int schedule_item_list=0x7f080008;
+        public static final int schedule_item_price=0x7f08000a;
+        public static final int schedule_item_remove=0x7f08000c;
+        public static final int schedule_start_date=0x7f080005;
+        public static final int schedule_total_budget=0x7f080007;
+        public static final int tabsLayout=0x7f08000f;
+        public static final int tabsText=0x7f080010;
         public static final int textView1=0x7f080000;
-        public static final int title=0x7f08000b;
+        public static final int title=0x7f08000d;
     }
     public static final class layout {
         public static final int borrow_lend_view=0x7f030000;
@@ -60,6 +62,7 @@ public final class R {
         public static final int menu_settings=0x7f040002;
         public static final int schedule_budget=0x7f040006;
         public static final int schedule_duration=0x7f040009;
+        public static final int schedule_edit_title=0x7f04000d;
         public static final int schedule_end_day=0x7f040005;
         public static final int schedule_living_cost=0x7f040007;
         public static final int schedule_monthly=0x7f04000a;
--- wip/Sources/trunk/PFM/src/money/Tracker/repository/ScheduleRepository.java
+++ wip/Sources/trunk/PFM/src/money/Tracker/repository/ScheduleRepository.java
@@ -7,11 +7,12 @@ public class ScheduleRepository implements IDataRepository {
 	
 	public ScheduleRepository() 
 	{
+		createTable();
 	}
 
-	protected void getData() {
+	protected void createTable() {
 		SqlHelper.instance.createTable("Schedule",
-				new StringBuilder("id INTEGER PRIMARY KEY, Budget FLOAT,")
+				new StringBuilder("Id INTEGER PRIMARY KEY, Budget FLOAT,")
 						.append("Start_date DATE, End_date DATE").toString());
 	}
 }
