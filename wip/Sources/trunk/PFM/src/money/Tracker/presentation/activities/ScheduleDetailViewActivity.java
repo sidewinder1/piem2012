fce1e70398764711c26f06b5f95481f1c31039cd
locnd01006@fpt.edu.vn <locnd01006@fpt.edu.vn@15378052-1c41-e357-49ac-df63ba3f61f7>
2012-09-30 17:54:41 +0000

    update sds

---
+++ wip/Sources/trunk/PFM/src/money/Tracker/presentation/activities/ScheduleDetailViewActivity.java
@@ -0,0 +1,13 @@
+package money.Tracker.presentation.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduleDetailViewActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.schedule_view_detail);
+	}
+}
--- wip/Sources/trunk/PFM/src/money/Tracker/presentation/customviews/ScheduleViewItem.java
+++ wip/Sources/trunk/PFM/src/money/Tracker/presentation/customviews/ScheduleViewItem.java
@@ -1,14 +1,19 @@
 package money.Tracker.presentation.customviews;
 
 import money.Tracker.presentation.activities.R;
+import money.Tracker.presentation.activities.ScheduleDetailViewActivity;
+import money.Tracker.presentation.activities.ScheduleMainViewActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ScheduleViewItem extends LinearLayout {
 	public TextView schedule_item_title, total_budget;
 	public LinearLayout stacked_bar_chart;
+	Intent viewDetail;
 	
 	public ScheduleViewItem(Context context) {
 		super(context);
@@ -20,5 +25,13 @@ public class ScheduleViewItem extends LinearLayout {
 		schedule_item_title = (TextView)findViewById(R.id.schedule_item_name);
 		total_budget = (TextView)findViewById(R.id.schedule_total_budget);
 		stacked_bar_chart = (LinearLayout)findViewById(R.id.stacked_bar_chart);
+		
+		this.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 }
