c81a8f24e70bc10cd4e42934907028205495f054
locnd01006@fpt.edu.vn <locnd01006@fpt.edu.vn@15378052-1c41-e357-49ac-df63ba3f61f7>
2012-10-04 05:53:19 +0000

    updated code

--- wip/Sources/trunk/PFM/src/money/Tracker/common/utilities/Alert.java
+++ wip/Sources/trunk/PFM/src/money/Tracker/common/utilities/Alert.java
@@ -1,8 +1,9 @@
 package money.Tracker.common.utilities;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.widget.Toast;
 
 public class Alert {
@@ -13,8 +14,28 @@ public class Alert {
 		if (instance == null) {
 			instance = new Alert();
 		}
-		
+
 		return instance;
+	}
+
+	public void showDialog(Context context, String message,
+			OnClickListener okAction) {
+		showDialog(context, message, okAction,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+	}
+
+	public void showDialog(Context context, String message,
+			OnClickListener okAction, OnClickListener cancelAction) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setMessage(message).setCancelable(false)
+				.setPositiveButton("Yes", okAction)
+				.setNegativeButton("No", cancelAction);
+		AlertDialog alert = builder.create();
+		alert.show();
 	}
 
 	public boolean show(Context context, String message) {
