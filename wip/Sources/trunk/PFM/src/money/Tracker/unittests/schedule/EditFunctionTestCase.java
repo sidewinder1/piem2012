73921b4fef04d28f5d7139e1ff58ae37de938269
locnd01006 <locnd01006@fpt.edu.vn>
2012-10-29 11:48:25 +0000

    update unit test

---
+++ wip/Sources/trunk/PFM/src/money/Tracker/unittests/schedule/EditFunctionTestCase.java
@@ -0,0 +1,12 @@
+package money.Tracker.unittests.schedule;
+
+import junit.framework.TestCase;
+
+public class EditFunctionTestCase extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	
+}
